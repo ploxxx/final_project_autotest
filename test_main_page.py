52f296aaec8e6b02e5b4f47ce83cb85568c0c3ff
Vlad Zharkov <zharkov.kill@yandex.by>
2022-07-25 17:28:58 +0300

    last fixs

--- test_main_page.py
+++ test_main_page.py
@@ -20,7 +20,6 @@ def test_guest_can_go_to_login_page(browser):
     login_page = LoginPage(browser,browser.current_url)
     login_page.should_be_login_page()
 
-@pytest.mark.new
 def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
     link = "http://selenium1py.pythonanywhere.com"
     page = BasePage(browser,link)
--- test_product_page.py
+++ test_product_page.py
@@ -13,6 +13,7 @@ from pages.product_page import ProductPage
 from pages.base_page import BasePage
 from pages.basket_page import BasketPage
 
+@pytest.mark.need_review
 @pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
@@ -31,20 +32,21 @@ def test_guest_can_add_product_to_cart(browser,link):
     page.should_be_message_about_adding()
     page.should_be_message_basket_total() 
 
-
+@pytest.mark.need_review
 def test_guest_should_see_login_link_on_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link)
     page.open()
     page.should_be_login_link()
 
+@pytest.mark.need_review
 def test_guest_can_go_to_login_page_from_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link)
     page.open()
     page.go_to_login_page()
 
-@pytest.mark.new
+@pytest.mark.need_review
 def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com"
     page = BasePage(browser,link)
